d1085d10fffc16756dfc5a0674e12de7a146da34
delvauxo <delvauxo@outlook.com>
2021-11-29 23:33:28 +0100

    TP-03 - Structures - Day of the week

---
+++ 03-JavaScript/TP-03-Structures/main.js
@@ -0,0 +1,42 @@
+function main() {
+    
+    let date = new Date()
+    console.log(date)
+
+    let day = date.getDay()
+    console.log(day)
+
+    let dayFull
+
+    // Bonjour, nous sommes lundi!
+    switch(day) {
+        case 1:
+            dayFull = 'lundi'
+            break
+        case 2:
+            dayFull = 'mardi'
+            break
+        case 3:
+            dayFull = 'mercredi'
+            break
+        case 4:
+            dayFull = 'jeudi'
+            break
+        case 5:
+            dayFull = 'vendredi'
+            break
+        case 6:
+            dayFull = 'samedi'
+            break
+        case 7:
+            dayFull = 'dimanche'
+            break
+        default:
+            console.log('Je ne sais pas quel jour nous sommes!')
+    }
+
+    console.log('Bonjour, nous sommes ' + dayFull + '!')
+
+}
+
+main()
