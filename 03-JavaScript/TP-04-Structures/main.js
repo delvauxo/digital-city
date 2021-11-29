3d09543c675f716b5eee6c70423c8bbbdb9e8702
delvauxo <delvauxo@outlook.com>
2021-11-29 23:54:37 +0100

    TP-04 - Structures - repeat string fixed first iteration having empty string

--- 03-JavaScript/TP-04-Structures/main.js
+++ 03-JavaScript/TP-04-Structures/main.js
@@ -3,7 +3,7 @@ function main() {
     const lines = 10
     const letter = 'A'
 
-    for (let i = 0; i <= lines; i++) {
+    for (let i = 1; i <= lines; i++) {
         console.log(letter.repeat(i))
     }
 
