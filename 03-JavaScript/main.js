cb9351581c0842080802f784b5d2b458e9fa9e5e
Delvauxo <1454755+delvauxo@users.noreply.github.com>
2021-11-29 16:13:13 +0100

    js name revert form

--- 03-JavaScript/main.js
+++ 03-JavaScript/main.js
@@ -1,7 +1,16 @@
-console.log('Hello World')
-
-let couleur1 = "bleu"
-let couleur2 = "blanc"
-let couleur3 = "rouge"
-
-console.log('Hello World !!!!!!')
+function reverseName() {
+    function getName() {
+        let yourName = document.getElementById('yourName').value
+        console.log(yourName)
+        return yourName
+    }
+    let name = getName()
+    if (name) {
+        let reversedName = name.split('').reverse('').join('')
+        console.log(reversedName)
+    }
+    function displayName() {
+        let el = document.getElementById('name-reversed')
+        el.innerHTML = name
+    }
+}
