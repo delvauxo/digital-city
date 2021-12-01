f22a8ccf87d3a6073fc84b4777662fca81e49370
delvauxo <delvauxo@outlook.com>
2021-12-01 17:51:49 +0100

    word count and average points functions

--- 03-JavaScript/_word-count-sentence/main.js
+++ 03-JavaScript/_word-count-sentence/main.js
@@ -15,17 +15,32 @@ let frequence = function (sentence) {
     return count
 }
 
+// ------------------------------------------------
+// Average points
+// ------------------------------------------------
+
 let student1 = {
-    nom: 'Jean',
-    note: [15, 16, 18]
+    name: 'Jean',
+    points: [15, 16, 18, 2]
 }
 let student2 = {
-    nom: 'Marc',
-    note: [6, 18, 20]
+    name: 'Marc',
+    points: [6, 18, 20, 18]
 }
+
+let pointsAvg = function (points) {
+    let avg = 0
+    for (let i = 0; i < points.length; i++) {
+        avg += points[i]
+    }
+    avg = Math.round(avg/points.length)
+    return avg
+}
+
+console.log(pointsAvg(student2.points))
 
 let isBetter = function (student1, student2) {
-    
+    return pointsAvg(student1.points) > pointsAvg(student2.points)
 }
 
-isBetter()
+console.log(isBetter(student1, student2))
