3bf2ecb11fd475208e338f92cd5b420a2719da9a
delvauxo <delvauxo@outlook.com>
2021-12-20 11:44:25 +0100

    casino bet btn and play btn

--- 03-JavaScript/_casino/app.js
+++ 03-JavaScript/_casino/app.js
@@ -1,12 +1,47 @@
-// 3 x image 1 = mise * 2
-// 3 x image 2 = mise * 2
-// 3 x image 3 = mise * 3
-// 3 x image 4 = mise * 5
-// 3 x image 5 = mise * 10
-// 3 x image 6 = mise * 20
+// 3 x image 1 = bet * 2
+// 3 x image 2 = bet * 2
+// 3 x image 3 = bet * 3
+// 3 x image 4 = bet * 5
+// 3 x image 5 = bet * 10
+// 3 x image 6 = bet * 20
 
-let machine = 1000000;
-let pot = 10000;
-const mise = 0;
+(function main() {
 
+    let machine = 1000000
+    let pot = 10000
+    let bet = 0
+    
+    // Selectors.
+    const btnBets = document.querySelectorAll('#btn-bets .btn')
+    const btnPlay = document.querySelector('#btn-play')
+    const resultWarning = document.querySelector('#result #result-warning')
+    const resultDisplay = document.querySelector('#result #result-display')
 
+    for (const btn of btnBets) {
+        btn.addEventListener('click', function (e) {
+            // Set bet.
+            bet = parseInt(this.dataset.bet)
+            // Remove class from current active button.
+            btnBets.forEach(item => {
+                item.classList.remove('btn-bet')
+            });
+            // Set class on active bet button.
+            this.classList.toggle('btn-bet')
+            // Set class on HTML element (pick a bet).
+            resultWarning.classList.add('hide')
+        })
+    }
+
+    btnPlay.addEventListener('click', function () {
+        console.log('play')
+        if (bet !== 0) {
+            console.log('bet : ', bet)
+            resultDisplay.textContent = 'your bet : ' + bet
+        } else {
+            resultWarning.classList.remove('hide')
+            console.log('please choose your bet')
+        }
+    })
+    
+
+})()
