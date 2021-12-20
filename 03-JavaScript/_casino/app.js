5c18709f7c24289de54d04cdfb2d5e99a7ba0f5d
delvauxo <delvauxo@outlook.com>
2021-12-20 09:52:50 +0100

    casino cleaning start files and setting sass

---
+++ 03-JavaScript/_casino/app.js
@@ -0,0 +1,12 @@
+// 3 x image 1 = mise * 2
+// 3 x image 2 = mise * 2
+// 3 x image 3 = mise * 3
+// 3 x image 4 = mise * 5
+// 3 x image 5 = mise * 10
+// 3 x image 6 = mise * 20
+
+let machine = 1000000;
+let pot = 10000;
+const mise = 0;
+
+
