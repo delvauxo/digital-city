7d802c6f3c57a138c40834de1a1e98d3877f485c
Delvauxo <1454755+delvauxo@users.noreply.github.com>
2022-02-18 10:19:53 +0100

    nodejs

---
+++ 10-NodeJS/modueHTTP/server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer()
+
+server.on('request', (request, response) => {
+    
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            response.writeHead(404)
+            response.end("Ce fichier n'existe pas !")
+        } else {
+            response.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'
+            })
+            response.end(data)
+        }
+    })
+
+    console.log('Il y a eu une requete !')
+
+})
+
+server.listen(8080)
